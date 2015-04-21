c834feb2e68361b3a7b052087c433626d8a6ce6a
Alain E Hernandez <alainh@plus.seas.upenn.edu>
2015-04-20 20:24:19 -0400

    Added python wrapper with requests to minimize calls to shell

---
+++ recognizer.py
@@ -0,0 +1,21 @@
+import subprocess
+import requests
+
+def recognize():
+    subprocess.call("streamer -f -o picture.jpeg")
+    url = "http://demo.caffe.berkeleyvision.org/classify_upload"
+    img = {'name': 'picture','imagefile': open('picture.jpg', 'rb')}
+    r = requests.post(url, files=img)
+    document = r.text
+
+    a = 0
+    b = 0
+
+    found = []
+    for i in range(0, 10):
+        a = document[b:].find('target="_blank">') + b
+        b = document[a:].find('</a>') + a
+        item = document[(a+16):b]
+        found.append(item)
+        print item
+    return found
